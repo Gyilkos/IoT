33692f6f01e99f566b7ee19605b89df1f1ecacc9
Gyilkos <nicolas.robles@alumnos.ucn.cl>
2023-04-11 12:09:06 -0400

    arreglo proyectos

--- ServoMotor/ServoMotor.ino
+++ ServoMotor/ServoMotor.ino
@@ -4,29 +4,23 @@
 
 Servo miServo;
 
-int valor;
-int angulo;
-
 void setup() {
   // put your setup code here, to run once:
 
   miServo.attach(9);
-  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  valor = analogRead(POT);
-  Serial.print("valor: ");
-  Serial.print(valor);
-
-  angulo = map(valor, 0, 1023, 0, 180);
-
-  Serial.print(", angulo: ");
-  Serial.print(angulo);
-  miServo.write(angulo);
-
-  delay(15);
+  miServo.write(0);
+  delay(1000);
+  miServo.write(90);
+  delay(1000);
+  miServo.write(180);
+  delay(1000);
+  
+  miServo.write(90);
+  delay(1000);
 }
